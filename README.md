c589211e42f752677ce6938d6454d6c6b0072d52
Tracy Hebrew <tracyhebrew@gmail.com>
2017-09-06 15:05:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## HTML Starter
+
+Requires Node.js and Gulp installation
+
+Compiling Styles and Browsersync
+- `npm install`
+- `gulp dev` - Site runs locally at localhost:3000
+
---
+++ src/js/modules/theme.js
@@ -0,0 +1,38 @@
+// ------------------------------------
+//
+// Theme
+//
+// ------------------------------------
+
+(function($) {
+
+    if (typeof window.Theme == 'undefined') window.Theme = {};
+
+    Theme = {
+
+        settings: {},
+
+        /*
+         * Theme init
+         */
+
+        init: function() {
+
+            // Init 
+            
+            this.mobileMenu();
+
+
+        },
+
+        mobileMenu: function() {
+
+        },
+
+
+    };
+    
+    module.exports = Theme;
+
+
+})(jQuery);
